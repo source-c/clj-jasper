120937e4e7650c54df910492865fb59d0b500cd6
MelKori <mieldo@gmail.com>
2019-04-11 15:47:34 +0300

    update deps

--- profiles.clj
+++ profiles.clj
@@ -5,5 +5,5 @@
            :clean-targets  ^{:protect false} [:target-path]
 
            :plugins        [[lein-ancient "0.6.15"]]
-           :dependencies   [[org.clojure/clojure "1.9.0"]]}
+           :dependencies   [[org.clojure/clojure "1.10.0"]]}
  :uberjar {:aot :all :jvm-opts ["-Xmx1G"]}}
--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject net.tbt-post/clj-jasper "0.0.3"
+(defproject net.tbt-post/clj-jasper "0.0.4"
   :description "A Clojure library to work with Jasper Reports"
   :url "https://github.com/source-c/clj-jasper"
   :license {:name "MIT License"}
 
   :dependencies [;; Jasper Libs
-                 [net.sf.jasperreports/jasperreports "6.7.0"
+                 [net.sf.jasperreports/jasperreports "6.8.0"
                   :exclusions [com.lowagie/itext]]
-                 [net.sf.jasperreports/jasperreports-fonts "6.0.0"]
+                 [net.sf.jasperreports/jasperreports-fonts "6.8.0"]
                  ;[com.lowagie/itext "4.2.2"]
                  [com.lowagie/itext "2.1.7"]
                  [net.sourceforge.barbecue/barbecue "1.5-beta1"]
@@ -15,8 +15,8 @@
                  ;; Loggers
                  ;; -------
                  [com.taoensso/timbre "4.10.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.12"]
-                 [org.slf4j/slf4j-api "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]])
+                 [com.fzakaria/slf4j-timbre "0.3.13"]
+                 [org.slf4j/slf4j-api "1.7.26"]
+                 [org.slf4j/log4j-over-slf4j "1.7.26"]
+                 [org.slf4j/jul-to-slf4j "1.7.26"]
+                 [org.slf4j/jcl-over-slf4j "1.7.26"]])
