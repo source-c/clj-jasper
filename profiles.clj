233af54aa0d66ff295c90df81d00790cf2357901
MelKori <mieldo@gmail.com>
2019-04-12 18:25:48 +0300

    remove loggers from production; bump version

--- profiles.clj
+++ profiles.clj
@@ -5,5 +5,13 @@
            :clean-targets  ^{:protect false} [:target-path]
 
            :plugins        [[lein-ancient "0.6.15"]]
-           :dependencies   [[org.clojure/clojure "1.10.0"]]}
+           :dependencies   [[org.clojure/clojure "1.10.0"]
+                            ;; Loggers
+                            ;; -------
+                            [com.taoensso/timbre "4.10.0"]
+                            [com.fzakaria/slf4j-timbre "0.3.13"]
+                            [org.slf4j/slf4j-api "1.7.26"]
+                            [org.slf4j/log4j-over-slf4j "1.7.26"]
+                            [org.slf4j/jul-to-slf4j "1.7.26"]
+                            [org.slf4j/jcl-over-slf4j "1.7.26"]]}
  :uberjar {:aot :all :jvm-opts ["-Xmx1G"]}}
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.tbt-post/clj-jasper "0.0.4"
+(defproject net.tbt-post/clj-jasper "0.0.5"
   :description "A Clojure library to work with Jasper Reports"
   :url "https://github.com/source-c/clj-jasper"
   :license {:name "MIT License"}
@@ -12,13 +12,4 @@
                  [net.sourceforge.barbecue/barbecue "1.5-beta1"]
                  [com.google.zxing/core "3.3.3"]
                  [net.sf.barcode4j/barcode4j "2.1"]
-                 [org.apache.xmlgraphics/batik-bridge "1.11"]
-
-                 ;; Loggers
-                 ;; -------
-                 [com.taoensso/timbre "4.10.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.13"]
-                 [org.slf4j/slf4j-api "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]])
+                 [org.apache.xmlgraphics/batik-bridge "1.11"]])
