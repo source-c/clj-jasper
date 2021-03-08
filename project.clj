a394168b667875c83f642b4d2e46576f754409d4
MelKori <mieldo@gmail.com>
2021-03-08 18:05:53 +0200

    updated jasperreports to 6.16.0

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject net.tbt-post/clj-jasper "0.0.7"
+(defproject net.tbt-post/clj-jasper "0.0.8"
   :description "A Clojure library to work with Jasper Reports"
   :url "https://github.com/source-c/clj-jasper"
   :license {:name "MIT License"}
 
   :dependencies [;; Jasper Libs
-                 [net.sf.jasperreports/jasperreports "6.12.2"
+                 [net.sf.jasperreports/jasperreports "6.16.0"
                   :exclusions [com.lowagie/itext]]
-                 [net.sf.jasperreports/jasperreports-fonts "6.0.0"]
+                 [net.sf.jasperreports/jasperreports-fonts "6.16.0"]
                  [com.lowagie/itext "2.1.7"]
                  [net.sourceforge.barbecue/barbecue "1.5-beta1"]
-                 [com.google.zxing/core "3.4.0"]
+                 [com.google.zxing/core "3.4.1"]
                  [net.sf.barcode4j/barcode4j "2.1"]
-                 [org.apache.xmlgraphics/batik-bridge "1.13"]])
+                 [org.apache.xmlgraphics/batik-bridge "1.14"]])
