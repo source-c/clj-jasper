2e9b5246ec469c94dd2a72038b9f34baa77a6950
MelKori <mieldo@gmail.com>
2022-02-11 21:53:56 +0200

    update main libs

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject net.tbt-post/clj-jasper "0.0.8"
+(defproject net.tbt-post/clj-jasper "0.0.9"
   :description "A Clojure library to work with Jasper Reports"
   :url "https://github.com/source-c/clj-jasper"
   :license {:name "MIT License"}
 
   :dependencies [;; Jasper Libs
-                 [net.sf.jasperreports/jasperreports "6.16.0"
-                  :exclusions [com.lowagie/itext]]
-                 [net.sf.jasperreports/jasperreports-fonts "6.16.0"]
-                 [com.lowagie/itext "2.1.7"]
+                 [net.sf.jasperreports/jasperreports "6.18.1"]
+                 [net.sf.jasperreports/jasperreports-fonts "6.18.1"]
+                 ;[com.lowagie/itext "2.1.7"]
                  [net.sourceforge.barbecue/barbecue "1.5-beta1"]
                  [com.google.zxing/core "3.4.1"]
                  [net.sf.barcode4j/barcode4j "2.1"]
