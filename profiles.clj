8f8468d6c136ba2cfe9347bf18f279342bf04b50
MelKori <mieldo@gmail.com>
2022-02-11 21:55:18 +0200

    update development deps

--- profiles.clj
+++ profiles.clj
@@ -4,14 +4,13 @@
            :target-path    "target/%s"
            :clean-targets  ^{:protect false} [:target-path]
 
-           :plugins        [[lein-ancient "0.6.15"]]
-           :dependencies   [[org.clojure/clojure "1.10.0"]
+           :dependencies   [[org.clojure/clojure "1.10.3"]
                             ;; Loggers
                             ;; -------
-                            [com.taoensso/timbre "4.10.0"]
-                            [com.fzakaria/slf4j-timbre "0.3.13"]
-                            [org.slf4j/slf4j-api "1.7.26"]
-                            [org.slf4j/log4j-over-slf4j "1.7.26"]
-                            [org.slf4j/jul-to-slf4j "1.7.26"]
-                            [org.slf4j/jcl-over-slf4j "1.7.26"]]}
+                            [com.taoensso/timbre "5.1.2"]
+                            [com.fzakaria/slf4j-timbre "0.3.21"]
+                            [org.slf4j/slf4j-api "1.7.36"]
+                            [org.slf4j/log4j-over-slf4j "1.7.36"]
+                            [org.slf4j/jul-to-slf4j "1.7.36"]
+                            [org.slf4j/jcl-over-slf4j "1.7.36"]]}
  :uberjar {:aot :all :jvm-opts ["-Xmx1G"]}}
